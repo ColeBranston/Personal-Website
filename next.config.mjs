a390ed463272637cd3ee20b5a505df2b32e13b2c
Cole Branston <colebranston@gmail.com>
2026-07-08 23:37:21 -0400

    updating static file serving for github website links

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: isGithubPages ? 'export' : undefined,
+  trailingSlash: true,
+  basePath: isGithubPages ? '/colebranston.github.io' : '',
+  assetPrefix: isGithubPages ? '/colebranston.github.io/' : '',
   images: {
+    unoptimized: isGithubPages ? true : false,
     remotePatterns: [
-      { protocol: "https", hostname: "opengraph.githubassets.com" },
-      { protocol: "https", hostname: "avatars.githubusercontent.com" },
-      { protocol: "https", hostname: "raw.githubusercontent.com" },
-      { protocol: "https", hostname: "github.com" },
+      { protocol: 'https', hostname: 'opengraph.githubassets.com' },
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+      { protocol: 'https', hostname: 'raw.githubusercontent.com' },
+      { protocol: 'https', hostname: 'github.com' },
     ],
   },
 };
